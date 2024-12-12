df8900f965e54d96789bd1ed18662bc22a558711
Dwarkesh <dwarkeshgohel1@gmail.com>
2024-12-12 17:08:15 +0530

    pwa added

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,18 @@
+import withPWA from "next-pwa";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ["lh3.googleusercontent.com", "i.scdn.co"],
   },
-  async redirects() {
-    return [
-      {
-        source: "/auth/signup",
-        destination: "/auth/signin",
-        permanent: false,
-      },
-    ];
-  },
 };
 
-export default nextConfig;
+export default withPWA({
+  ...nextConfig,
+  dest: "public",
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development
+  register: true,
+  skipWaiting: true,
+});
