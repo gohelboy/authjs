b72f705c776ad5e3379e6caa956846ffaea844a6
Dwarkesh <dwarkeshgohel1@gmail.com>
2024-12-12 17:15:42 +0530

    mjs to js

---
+++ next.config.js
@@ -0,0 +1,18 @@
+// next.config.js
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ["lh3.googleusercontent.com", "i.scdn.co"], // Add your image domains here
+  },
+};
+
+module.exports = withPWA(nextConfig);
