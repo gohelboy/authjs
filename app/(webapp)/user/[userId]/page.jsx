c2c691ae75b0f92f23b0da8861681a81f65490ba
Dwarkesh <dwarkeshgohel1@gmail.com>
2024-12-16 13:13:08 +0530

    profile ui change

--- app/(webapp)/user/[userId]/page.jsx
+++ app/(webapp)/user/[userId]/page.jsx
@@ -1,7 +1,13 @@
 "use client";
 
+import NowPlaying from "@/components/custom/NowPlaying";
+import PlaybackHistory from "@/components/custom/PlaybackHistory";
+import TopArtists from "@/components/custom/TopArtists";
+import TopTracks from "@/components/custom/TopTracks";
 import { Button } from "@/components/ui/button";
-import { Users, UsersRound } from "lucide-react";
+import { TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tabs } from "@radix-ui/react-tabs";
+import { Clock, Headphones, Music, PlayCircle, Users, UsersRound } from "lucide-react";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
@@ -14,6 +20,19 @@ const UserProfilePage = ({ params }) => {
   const [loading, setLoading] = useState(true);
   const [isFollowed, setIsFollowed] = useState(false);
   const [loadingFollow, setLoadingFollow] = useState(false);
+
+
+  const tabs = [
+    {
+      value: "currently-playing",
+      icon: PlayCircle,
+      label: "Now Playing",
+    },
+    { value: "top-tracks", icon: Music, label: "Top Tracks" },
+    { value: "top-artists", icon: Headphones, label: "Top Artists" },
+    { value: "playback-history", icon: Clock, label: "Play History" },
+
+  ];
 
   const fetchUserProfile = async () => {
     try {
@@ -59,44 +78,67 @@ const UserProfilePage = ({ params }) => {
 
   return (
     <div className="max-w-6xl mx-auto">
-      <div className="rounded-lg p-8 border border-neutral-800 mx-4">
-        <div className="grid md:grid-cols-3 gap-8">
+      <div className="rounded-lg p-8 border border-neutral-800 mx-4 md:mx-0 my-4">
+        <div className="grid md:grid-cols-3 md:gap-8">
           {/* Profile Image */}
-          <div className="flex flex-col items-center justify-center">
+          <div className="flex gap-8 items-center  md:justify-center ">
             <Image
               src={profileData?.image || "/user.jpg"}
               alt="Profile"
               width={154}
               height={154}
-              className="rounded-full border-4 border-green-500 shadow-lg mb-4"
+              className="rounded-full size-28 md:size-36 border-4 border-green-500 shadow-lg mb-4"
             />
-            <h2 className="text-2xl font-semibold tracking-tight">
-              {profileData?.name || "User"}
-            </h2>
-
-            {session.user.email !== profileData?.email && (
-              <Button
-                onClick={toggleFollow}
-                className={`flex items-center justify-center rounded-lg font-semibold hover:text-white bg-white text-neutral-900  mt-2`}
-                disabled={loadingFollow}
-              >
-                {loadingFollow ? (
-                  <span
-                    className={`loader inline-block w-4 h-4 border-2 rounded-full animate-spin ${
-                      isFollowed ? "border-t-white" : "border-t-neutral-900"
-                    }`}
-                  ></span>
-                ) : isFollowed ? (
-                  "Unfollow"
-                ) : (
-                  "Follow"
-                )}
-              </Button>
-            )}
+            <div className="flex items-center md:gap-3 md:flex-row flex-col md:hidden">
+              <h2 className="text-2xl font-semibold tracking-tight">
+                {profileData?.name || "User"}
+              </h2>
+              {session.user.email !== profileData?.email && (
+                <Button
+                  onClick={toggleFollow}
+                  className={`flex items-center justify-center rounded-lg font-semibold hover:text-white bg-white text-neutral-900  mt-2`}
+                  disabled={loadingFollow}
+                >
+                  {loadingFollow ? (
+                    <span
+                      className={`loader inline-block w-4 h-4 border-2 rounded-full animate-spin ${isFollowed ? "border-t-white" : "border-t-neutral-900"
+                        }`}
+                    ></span>
+                  ) : isFollowed ? (
+                    "Unfollow"
+                  ) : (
+                    "Follow"
+                  )}
+                </Button>
+              )}
+            </div>
           </div>
 
           {/* Profile Details */}
           <div className="md:col-span-2 space-y-6">
+            <div className="items-center gap-3 hidden md:flex">
+              <h2 className="text-2xl font-semibold tracking-tight">
+                {profileData?.name || "User"}
+              </h2>
+              {session.user.email !== profileData?.email && (
+                <Button
+                  onClick={toggleFollow}
+                  className={`flex items-center justify-center rounded-lg font-semibold hover:text-white bg-white text-neutral-900  mt-2`}
+                  disabled={loadingFollow}
+                >
+                  {loadingFollow ? (
+                    <span
+                      className={`loader inline-block w-4 h-4 border-2 rounded-full animate-spin ${isFollowed ? "border-t-white" : "border-t-neutral-900"
+                        }`}
+                    ></span>
+                  ) : isFollowed ? (
+                    "Unfollow"
+                  ) : (
+                    "Follow"
+                  )}
+                </Button>
+              )}
+            </div>
             <div className="grid grid-cols-2 gap-6">
               <Link
                 href={`/user/${userId}/followers`}
@@ -123,6 +165,51 @@ const UserProfilePage = ({ params }) => {
           </div>
         </div>
       </div>
+
+
+      <div>
+        <Tabs defaultValue="currently-playing" className="w-full ">
+          <TabsList className="grid w-full grid-cols-4 bg-neutral-800 min-h-12 p-2 mb-3">
+            {tabs.map(({ value, icon: Icon, label }) => (
+              <TabsTrigger
+                key={value}
+                value={value}
+                className="flex items-center space-x-2 justify-center h-full"
+              >
+                <Icon size={16} />
+                <span className="hidden md:block">{label}</span>
+              </TabsTrigger>
+            ))}
+          </TabsList>
+
+          <TabsContent
+            value="currently-playing"
+            className="bg-neutral-800 rounded-2xl p-6 shadow-2xl"
+          >
+            <NowPlaying />
+          </TabsContent>
+          <TabsContent
+            value="top-tracks"
+            className="bg-neutral-800 rounded-2xl shadow-2xl"
+          >
+            <TopTracks />
+          </TabsContent>
+          <TabsContent
+            value="top-artists"
+            className="bg-neutral-800 rounded-2xl shadow-2xl"
+          >
+            <TopArtists />
+          </TabsContent>
+          <TabsContent
+            value="playback-history"
+            className="bg-neutral-800 rounded-2xl p-2 md:p-6 shadow-2xl"
+          >
+            <PlaybackHistory />
+          </TabsContent>
+
+        </Tabs>
+      </div>
+
     </div>
   );
 };
